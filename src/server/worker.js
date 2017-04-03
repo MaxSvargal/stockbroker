390f2eaf6a5044e2121b35cf8716297f075306eb
MaxSvargal <maxsvargal@gmail.com>
2017-04-03 23:52:56 +0300

    Add basic auth

--- src/server/worker.js
+++ src/server/worker.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import basicAuth from 'express-basic-auth'
 import createStore from './store'
 import handleRender from './renderer'
 
@@ -10,6 +11,7 @@ export const run = worker => {
   const app = express()
   const store = createStore(scServer)
 
+  app.use(basicAuth({ users: { stockbroker: 'toptrader88' } }))
   app.use(express.static('public', { maxAge: 1000 }))
   app.use(handleRender(store))
 
