bd7d62182de3a13d8a8c2ac64ce263161474360f
MaxSvargal <maxsvargal@gmail.com>
2017-04-04 03:04:44 +0300

    Enable babili minification

--- webpack.config.client.prod.js
+++ webpack.config.client.prod.js
@@ -1,11 +1,11 @@
 const webpack = require('webpack')
 const path = require('path')
-// const BabiliPlugin = require('babili-webpack-plugin')
+const BabiliPlugin = require('babili-webpack-plugin')
 // const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 /* eslint import/no-dynamic-require: 0 */
 const accountPath = path.resolve(__dirname, 'src/server/accounts', `${process.env.ACCOUNT}.json`)
-const accountConfig = require(accountPath)
+const { pair } = require(accountPath)
 
 module.exports = {
   entry: 'client/index.js',
@@ -24,11 +24,11 @@ module.exports = {
     // new HtmlWebpackPlugin({ title: 'PoloniexBot' })
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('development'),
+        NODE_ENV: JSON.stringify('production'),
         BROWSER: JSON.stringify(true),
-        ACCOUNT: JSON.stringify(accountConfig)
+        ACCOUNT: JSON.stringify({ pair })
       }
     }),
-    // new BabiliPlugin()
+    new BabiliPlugin({}, { comments: false })
   ]
 }
