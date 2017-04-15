7551e4635a3cae08782e35a0b1b1e3c78d3684b2
MaxSvargal <maxsvargal@gmail.com>
2017-04-15 19:25:11 +0300

    Render current result

--- src/client/components/FinalCurrentResult.js
+++ src/client/components/FinalCurrentResult.js
@@ -4,26 +4,41 @@ import { hh, div } from 'react-hyperscript-helpers'
 
 class FinalCurrentResult extends Component {
   render() {
-    const { statsResult = 0 } = this.props
-    const isPositive = statsResult >= 0
-    const chars = [ ...new Array(Math.abs(statsResult / 2)) ]
+    const { statsDynamicsTotal: { buyDynTotal, sellDynTotal } } = this.props
+
+    const isBuyPositive = buyDynTotal >= 0
+    const isSellPositive = sellDynTotal >= 0
+    const charsBuy = [ ...new Array(Math.abs(buyDynTotal / 2)) ]
+    const charsSell = [ ...new Array(Math.abs(sellDynTotal / 2)) ]
     const styles = this.getStyles()
 
-    return div({ style: styles.root },
-      chars.map((v, i) => div({ style: styles.char(isPositive, i) }, isPositive ? '▲' : '▼')))
+    return div({ style: styles.root }, [
+      div({ style: styles.child }, charsBuy.map((v, i) =>
+        div({ style: styles.char(isBuyPositive, i) }, isBuyPositive ? '▲' : '▼'))),
+
+      div({ style: styles.child }, charsSell.map((v, i) =>
+        div({ style: styles.char(isSellPositive, i) }, isSellPositive ? '▲' : '▼')))
+    ])
   }
 
   getStyles() {
     return {
       root: {
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
         position: 'absolute',
         margin: '-1.6rem 0 0 1rem',
         paddingTop: '0.15rem',
         height: '2rem'
+      },
+      child: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: '0 .5rem'
       },
       char: (isPositive, index) => ({
         color: isPositive ? `#72bf${index * 2}d` : `#c349${index * 2}a`,
@@ -35,5 +50,5 @@ class FinalCurrentResult extends Component {
   }
 }
 
-const mapStateToProps = ({ statsResult }) => ({ statsResult })
+const mapStateToProps = ({ statsDynamicsTotal }) => ({ statsDynamicsTotal })
 export default hh(connect(mapStateToProps)(FinalCurrentResult))
