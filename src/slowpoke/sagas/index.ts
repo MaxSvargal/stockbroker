d397bb85a712036572011dfade4fea967b1be087
MaxSvargal <maxsvargal@gmail.com>
2017-09-30 20:49:46 +0300

    Add current candle to stochastic calculation

--- src/slowpoke/sagas/index.ts
+++ src/slowpoke/sagas/index.ts
@@ -12,7 +12,7 @@ if (!PAIR || !AMOUNT) throw Error('No pair or amount will passed by environment'
 const amount = Number(AMOUNT)
 
 export function* analyticsSaga() {
-  let lastDStochastic
+  let lastDStochastics = [ 0, 0 ]
   while (true) {
     const candlesKey = `trade:5m:t${PAIR}`
     const candles = yield select(selectCandles, candlesKey, 14)
@@ -20,24 +20,40 @@ export function* analyticsSaga() {
     const highestHigh = yield select(selectHighestHigh, candlesKey, 14)
     const ticker = yield select(selectTicker, PAIR)
 
-    const lastCandles = <CandleData[]>candles.slice(candles.length - 4, candles.length - 1)
+    const lastCandles = <CandleData[]>candles.slice(candles.length - 3, candles.length)
     const [ bid, bidSize, ask, askSize, dailyChange, dailyChangePerc, lastPrice ] = ticker
     const lastStochastics = lastCandles.map(candle => stochasticOscillator(candle[2], lowestLow, highestHigh))
     const DStochastic = SMA(lastStochastics)
 
-    debug('worker')({ ACCOUNT, PAIR, DStochastic })
+    debug('worker')([ ACCOUNT, PAIR, DStochastic ])
+    // debug('worker')({ lastDStochastics, DStochastic })
 
-    if (lastDStochastic && lastDStochastic > 80 && DStochastic < 80) {
-      debug('worker')('Stochastic signal to sell by', bid)
+    if (
+      lastDStochastics[0] !== 0 &&
+      lastDStochastics[0] > 80 &&
+      lastDStochastics[1] > DStochastic &&
+      lastDStochastics[1] > 80 &&
+      DStochastic < 80
+    ) {
+      debug('worker')(`[${ACCOUNT}] Stochastic signal to sell -${amount} ${PAIR}`)
       yield put(execNewOrder({ symbol: `t${PAIR}`, amount: -amount, price: bid }))
     }
 
-    if (lastDStochastic && lastDStochastic < 20 && DStochastic > 20) {
-      debug('worker')('Stochastic signal to buy by', ask)
+    if (
+      lastDStochastics[0] !== 0 &&
+      lastDStochastics[0] < 20 &&
+      lastDStochastics[1] < DStochastic &&
+      lastDStochastics[1] < 20 &&
+      DStochastic > 20
+    ) {
+      debug('worker')(`[${ACCOUNT}] Stochastic signal to buy ${amount} ${PAIR}`)
       yield put(execNewOrder({ symbol: `t${PAIR}`, amount: amount, price: ask }))
     }
 
-    lastDStochastic = DStochastic
+    lastDStochastics = [
+      DStochastic !== lastDStochastics[1] ? lastDStochastics[1] : lastDStochastics[0],
+      DStochastic
+    ]
 
     yield delay(5000)
   }
