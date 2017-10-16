211fef41a2a09afb3999a57c89e37a7bbbdd3afa
MaxSvargal <maxsvargal@gmail.com>
2017-10-16 20:51:32 +0300

    Add LTCUSD for lesorub

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,15 +25,16 @@ const mcstoch = (account) => (pair, amount) => ({
 
 module.exports = {
   apps: [
-    exchanger('maxsvargal', [ 'BTCUSD' /*, 'LTCUSD', 'DSHUSD', 'ETHUSD', 'ETCUSD' */ ]),
-    // exchanger('lesorub', [ 'BTCUSD', 'ETCUSD' ]),
+    exchanger('maxsvargal', [ 'BTCUSD' ]),
+    exchanger('lesorub', [ 'LTCUSD' ]),
 
     mcstoch('maxsvargal')('BTCUSD', 0.005),
+    mcstoch('lesorub')('LTCUSD', 0.5),
+
     // slowpoke('maxsvargal')('DSHUSD', 0.05),
     // slowpoke('maxsvargal')('ETHUSD', 0.05),
     // slowpoke('maxsvargal')('LTCUSD', 1),
     // slowpoke('maxsvargal')('ETCUSD', 1),
-
     // slowpoke('lesorub')('BTCUSD', 0.02),
     // slowpoke('lesorub')('ETCUSD', 1),
   ]
