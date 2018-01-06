a26d6845eff475ce884bb964b5ec1d4333a750ad
MaxSvargal <maxsvargal@gmail.com>
2018-01-06 18:18:30 +0300

    Fix ramda cache of requests of binance listener

--- services/binance-listener/main.ts
+++ services/binance-listener/main.ts
@@ -1,13 +1,12 @@
 import debug from 'debug'
-import { apply, converge, call, __, o, zipWith, map, applyTo, props, flip, invoker, assoc, curry, compose, nth, CurriedFunction6 } from 'ramda'
+import { o, zipWith, map, props, flip, invoker, curry } from 'ramda'
 import { Stream, periodic, observe } from 'most'
 import { Requester, Publisher } from 'cote'
 
 type Candle = { openTime: number, open: string, close: string, high: string, low: string, volume: string }
 
-type PublishOf = CurriedFunction6<string, any, string, string, string, any, void>
-const publishOf: PublishOf = curry((event, publisher, key, symbol, frame, value) =>
-  publisher.publish(event, [ `${key}:${frame}:${symbol}`, value ]))
+const publishOf = curry((event: string, publisher: Publisher, key: string, symbol: string, frame: string, value: any) =>
+  publisher.publish(event, <any>[ `${key}:${frame}:${symbol}`, value ]))
 
 const limit = 50
 const candles = 'candles'
@@ -27,11 +26,12 @@ const main: Main = (exitProcess, mainLoopStream, binance, publisher, symbol) => 
   const getCandles = requestCandles(binance)
   const setFrameValue = publishSet(publisher, candles, symbol)
   const requestOptions = map(makeRequestOptions(limit, symbol), candlesFrames)
-
-  const candlesRequests = map(getCandles, requestOptions)
   const candlesResponses = o(zipWith(setFrameValue, candlesFrames), formatCandles)
 
-  const tick = () => Promise.all(candlesRequests).then(candlesResponses).catch(exitProcess)
+  const tick = () =>
+    Promise.all(map(getCandles, requestOptions))
+      .then(candlesResponses)
+      .catch(exitProcess)
 
   observe(tick, mainLoopStream)
 }
