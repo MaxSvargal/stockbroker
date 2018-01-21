f3369825864d73dad53f7bfc57f59d940450b333
MaxSvargal <maxsvargal@gmail.com>
2018-01-21 19:22:15 +0300

    Remove private keys from code

--- services/binance-account/index.ts
+++ services/binance-account/index.ts
@@ -1,11 +1,9 @@
 import { subscriberCons, requesterCons, binanceCons } from '../utils/cote'
 import main from './main'
 
-const {
-  ACCOUNT = 'maxsvargal',
-  KEY = 'ZtgjgCPqiKw80Y7M2BwzNjl2XspqiUCxWx5v1fRgDAnkWPhSpT5yNzbuaOT2SiqI',
-  SECRET = '8VP3B4wtMJcuGjWyDDwVDkZoeI16Z8pALYo2LDqXZm1loMKkVzmCkdbXrakdmXBC'
-} = process.env
+const { ACCOUNT, KEY, SECRET } = process.env
+
+console.log({ ACCOUNT, KEY, SECRET })
 
 const subscriber = subscriberCons({
   name: `Binance Exchange Account ${ACCOUNT}`,
