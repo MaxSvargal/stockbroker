5c737bde81fedf5b6ca5a0f3a2e7399964f8450a
MaxSvargal <maxsvargal@gmail.com>
2018-01-27 15:03:47 +0300

    Fix signals killer

--- services/signals-killer/main.ts
+++ services/signals-killer/main.ts
@@ -1,6 +1,6 @@
 import {
   map, prop, concat, flip, invoker, converge, pair, o, filter, lt, last, reject,
-  head, match, compose, not, isNil, uniq, unnest, difference, length, equals
+  head, match, compose, not, isNil, uniq, difference, length, equals, flatten
 } from 'ramda'
 import { Requester, Publisher } from 'cote'
 import { observe, Stream } from 'most'
@@ -8,17 +8,17 @@ import debug from 'debug'
 
 const invokeSend = flip(invoker(1, 'send'))
 const requestGetActiveSymbols = { type: 'cacheHashGetValues', key: 'accounts:activeSymbols' }
-const requestGetCurrentSymbol = { type: 'cacheHashGet', key: 'tradeState', field: 'currentSymbol' }
+const requestGetEnabledSymbols = { type: 'cacheHashGet', key: 'tradeState', field: 'enabledToBuySymbols' }
 const requestProcessesList = { type: 'processesList' }
 const requestStopSigProcess = (symbol: string) => ({ type: 'processStop', name: `Signal Publisher ${symbol}` })
 const requestStopLisProcess = (symbol: string) => ({ type: 'processStop', name: `Binance Exchange Listener ${symbol}` })
 
+const parse = flip(invoker(1, 'parse'))(JSON)
 const mapNamePidPair = map(converge(pair, [ prop('name'), prop('pid') ]))
 const filterOnlyActive = filter(o(lt(0), last))
 const mapSymbolsOfProc = map(compose(last, match(/Signal Publisher\s(.+)/), head))
 const removeNils = filter(o(not, isNil))
 const getActiveProcSymbols = compose(removeNils, mapSymbolsOfProc, filterOnlyActive, mapNamePidPair)
-const getUsersSymbols: (a: string[]) => string[] = compose(<any>uniq, unnest, <any>map(JSON.parse))
 const lengthIsMoreZero = o(lt(0), length)
 
 type Main = (a: (a: Event) => void, b: Stream<{}>, c: Requester, d: Requester) => void
@@ -28,14 +28,15 @@ const main: Main = (exitProcess, loopStream, requesterStore, requesterProcesses)
 
   const tick = async () => {
     try {
-      const procList = await processes(requestProcessesList)
-      const usersSymbols = await store(requestGetActiveSymbols)
-      const currentSymbol = await store(requestGetCurrentSymbol)
+      const [ procList, usersSymbolsRaw, enabledSymbolsRaw ] = await Promise.all([
+        processes(requestProcessesList),
+        store(requestGetActiveSymbols),
+        store(requestGetEnabledSymbols)
+      ])
 
       const activeProcesses = getActiveProcSymbols(procList)
-      const activeSymbols: string[] = getUsersSymbols(usersSymbols)
-      const diffOfActivities = difference(activeProcesses, activeSymbols)
-      const symbolsToStop = reject(equals(currentSymbol), diffOfActivities)
+      const activeSymbols: string[] = compose(uniq, flatten)([ map(parse, usersSymbolsRaw), parse(enabledSymbolsRaw) ])
+      const symbolsToStop = difference(activeProcesses, activeSymbols)
 
       if(lengthIsMoreZero(symbolsToStop)) {
         await Promise.all(concat(
