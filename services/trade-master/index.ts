d5dd32bfef7b1ee4f8b25659db83aa886121797f
MaxSvargal <maxsvargal@gmail.com>
2018-02-12 00:22:02 +0300

    Change sell to 5m candles

--- services/trade-master/index.ts
+++ services/trade-master/index.ts
@@ -27,6 +27,6 @@ const exitProcess = (err: Event) => {
   process.exit(1)
 }
 
-const loopStream = delay(5000, periodic(300000))
+const loopStream = delay(5000, periodic(240000))
 
 main(exitProcess, loopStream, fetch, requesterProcess, requesterDb, publisher)
