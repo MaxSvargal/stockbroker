085e40791887d76b29a5b709d67a4dd5da8ad28f
MaxSvargal <maxsvargal@gmail.com>
2018-02-16 19:31:46 +0300

    Decrease volatility percent

--- services/signaller/analysis.ts
+++ services/signaller/analysis.ts
@@ -45,7 +45,7 @@ const makeAnalysis: MakeAnalysis = (symbol: string) => ([ candles1m, candles5m ]
   const buySignal = buyPass({ low: last(lowLong), bb: bbShort, wr: wrShort })
   const sellSignal = sellPass({ high: last(highLong), bb: bbLong, wr: wrLong })
   const riftPrice = getLowestLow(lowLong)
-  const volatilityPerc = meanMargin(bbLong)
+  const volatilityPerc = meanMargin(bbLong) * o(prop('pb'), last)(bbLong)
 
   // log({
   //   now: new Date().toLocaleString(),
