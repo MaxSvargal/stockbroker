fa8436c89cd225a99171e7cf9e76cf5e003277e5
MaxSvargal <maxsvargal@gmail.com>
2018-02-16 18:40:17 +0300

    Remove unused microservices

--- services/signal-publisher/stochrsi.ts
+++
@@ -1,9 +0,0 @@
-import { reduce, takeLast, head, o, min, max, divide, subtract } from 'ramda'
-
-export default (values: number[], period: number = 14) => {
-  const last = takeLast(period, values)
-  const curr: number = o(head, takeLast(1))(last)
-  const low = reduce(min, Infinity)(last)
-  const high = reduce(max, -Infinity)(last)
-  return divide(subtract(curr, low), subtract(high, low))
-}
