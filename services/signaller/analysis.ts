ce4af1124499795cb608c86498d2a7415c2ee72d
MaxSvargal <maxsvargal@gmail.com>
2018-02-18 22:44:13 +0300

    Change Signaller buySignal low/bb check to 1m candles

--- services/signaller/analysis.ts
+++ services/signaller/analysis.ts
@@ -41,7 +41,7 @@ const makeAnalysis: MakeAnalysis = (symbol: string) => ([ candles1m, candles5m ]
   const wrLong = williamsr({ period: 14, close: closeLong, low: lowLong, high: highLong })
   const lastPrice = last(closeShort)
 
-  const buySignal = buyPass({ low: last(lowLong), bb: bbShort, wr: wrShort })
+  const buySignal = buyPass({ low: last(lowShort), bb: bbShort, wr: wrShort })
   const sellSignal = sellPass({ high: last(highLong), bb: bbLong, wr: wrLong })
   const riftPrice = getLowestLow(lowLong)
   const volatilityPerc = decreaseMeanMarginOnPb(bbLong)
